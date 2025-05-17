057cdc7ae8e2055ec1e36b7599a0907f6b5f5787
Anvar1234 <kingartaved@yandex.ru>
2025-05-17 18:46:21 +0500

    refactor: отрефакторил DbLineValidatorTest, заслал spy вместо mock - все работает

--- src/test/java/ru/yandex/kingartaved/validation/db_line_validator/DbLineValidatorTest.java
+++ src/test/java/ru/yandex/kingartaved/validation/db_line_validator/DbLineValidatorTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import ru.yandex.kingartaved.data.constant.NoteTypeEnum;
 
@@ -24,7 +25,8 @@ import static org.mockito.Mockito.*;
 public class DbLineValidatorTest {
 
     private static final String DB_FIELD_DELIMITER = "\\|";
-    @Mock
+
+    @Spy
     private ContentValidatorRegistry mockContentValidatorRegistry;
 
     @Mock
@@ -61,42 +63,6 @@ public class DbLineValidatorTest {
         verify(mockContentValidator).isValidContent("Sample text");
     }
 
-    @ParameterizedTest(name = "{1}")
-    @CsvFileSource(
-            resources = "/validate_db_line_structure_test_cases_should_fail.csv",
-            numLinesToSkip = 1)
-    @DisplayName("Проверка валидации некорректных строк из БД с лишними пробелами")
-    void validateDbLineStructure_extraSpacesPresent_failed(String lineFromCsv, String description) {
-        //given
-        String[] parts = lineFromCsv.split(DB_FIELD_DELIMITER);
-
-        //when
-        Executable actual = () -> dbLineValidator.validateDbLineStructure(parts);
-
-        //then
-        assertThrows(IllegalArgumentException.class, actual, description + " , строка должна быть невалидна");
-    }
-
-
-    @ParameterizedTest(name = "Валидация строки из БД для заметки типа: {1}")
-    @CsvSource({
-            // Корректная строка из БД для заметок типа TEXT_NOTE
-            "f47ac10b-58cc-4372-a567-0e02b2c3d479|Заметка 1|2023-10-01T12:34:56|2023-10-01T15:34:56|null|true|HIGH|ACTIVE|TEXT_NOTE|Содержимое,TEXT_NOTE",
-            // Корректная строка из БД для заметок типа CHECKLIST
-            "f47ac10b-58cc-4372-a567-0e02b2c3d479|Заметка 2|2023-10-01T12:34:56|2023-10-01T15:34:56|null|true|BASE|ACTIVE|CHECKLIST|1dsdsf:true;2dsfsf:false; 3d:false,CHECKLIST"
-    })
-    @DisplayName("Проверка валидации корректных строк БД")
-    void validateDbLineStructure_correctLine_success(String lineFromCsv, String noteType) {
-        //given
-        String[] parts = lineFromCsv.split(DB_FIELD_DELIMITER);
-
-        //when
-        Executable actual = () -> dbLineValidator.validateDbLineStructure(parts);
-
-        //then
-        assertDoesNotThrow(actual, "Для " + noteType + " строка должна быть валидна");
-    }
-
     @ParameterizedTest
     @MethodSource("provideContentTestCases")
     @DisplayName("Проверка валидации содержимого поля content (успешные и неуспешные сценарии)")
@@ -118,6 +84,41 @@ public class DbLineValidatorTest {
         }
     }
 
+    @ParameterizedTest(name = "{1}")
+    @CsvFileSource(
+            resources = "/db_line_validator/validateDbLineStructure_extraSpacesPresent_failed.csv",
+            numLinesToSkip = 1)
+    @DisplayName("Проверка валидации некорректных строк из БД с лишними пробелами")
+    void validateDbLineStructure_extraSpacesPresent_failed(String lineFromCsv, String description) {
+        //given
+        String[] parts = lineFromCsv.split(DB_FIELD_DELIMITER);
+
+        //when
+        Executable actual = () -> dbLineValidator.validateDbLineStructure(parts);
+
+        //then
+        assertThrows(IllegalArgumentException.class, actual, description + " , строка должна быть невалидна");
+    }
+
+    @ParameterizedTest(name = "Валидация строки из БД для заметки типа: {1}")
+    @CsvSource({
+            // Корректная строка из БД для заметок типа TEXT_NOTE
+            "f47ac10b-58cc-4372-a567-0e02b2c3d479|Заметка 1|2023-10-01T12:34:56|2023-10-01T15:34:56|null|true|HIGH|ACTIVE|TEXT_NOTE|Содержимое,TEXT_NOTE",
+            // Корректная строка из БД для заметок типа CHECKLIST
+            "f47ac10b-58cc-4372-a567-0e02b2c3d479|Заметка 2|2023-10-01T12:34:56|2023-10-01T15:34:56|null|true|BASE|ACTIVE|CHECKLIST|1dsdsf:true;2dsfsf:false; 3d:false,CHECKLIST"
+    })
+    @DisplayName("Проверка валидации корректных строк БД")
+    void validateDbLineStructure_correctLine_success(String lineFromCsv, String noteType) {
+        //given
+        String[] parts = lineFromCsv.split(DB_FIELD_DELIMITER);
+
+        //when
+        Executable actual = () -> dbLineValidator.validateDbLineStructure(parts);
+
+        //then
+        assertDoesNotThrow(actual, "Для " + noteType + " строка должна быть валидна");
+    }
+
     private static Stream<Arguments> provideContentTestCases() {
         return Stream.of(
                 Arguments.of("TEXT_NOTE", "Обычный текст", true),
@@ -129,7 +130,7 @@ public class DbLineValidatorTest {
 
     @ParameterizedTest(name = "{0}")
     @CsvFileSource(
-            resources = "/validate_test_cases_should_validate.csv",
+            resources = "/db_line_validator/validate_validAndInvalidContent_cases.csv",
             numLinesToSkip = 1)
     @DisplayName("Комплексная проверка валидации строки")
     void validate_validAndInvalidContent_cases(String description, String lineFromCsv, boolean expectedValid) {
